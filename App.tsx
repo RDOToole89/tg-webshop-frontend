8e4c5644fb8d78984f025df174cec824d4dac23f
RDOToole89 <roibinotoole@gmail.com>
2022-05-11 15:03:31 +0200

    revert previous commit => broke app

--- App.tsx
+++ App.tsx
@@ -20,7 +20,6 @@ export default function App() {
       "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
     ]);
 
-    LogBox.ignoreLogs(['Remote Debugger']);
     LogBox.ignoreLogs([
       'AsyncStorage has been extracted from react-native core and will be removed in a future release',
     ]);
